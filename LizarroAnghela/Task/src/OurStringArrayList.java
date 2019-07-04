4a73224120e518d623d58370038c91be3b2ff5f9
Anghela Lizarro <anghelalizarro7@gmail.com>
2019-07-04 00:20:32 -0400

    adding new task

---
+++ LizarroAnghela/Task/src/OurStringArrayList.java
@@ -0,0 +1,31 @@
+public class OurStringArrayList {
+    private String text;
+    private Integer number;
+    private Integer sizeNumber;
+    private Integer accumulator;
+    private String list;
+
+//methods
+
+    public String add() {
+        return text;
+    }
+
+    public void remove (Integer number){
+        this.number = number;
+
+    }
+
+    public Integer size (){
+        return sizeNumber;
+    }
+
+    public Integer get (){
+        return accumulator;
+    }
+
+    public String contains (){
+        return list;
+    }
+
+}

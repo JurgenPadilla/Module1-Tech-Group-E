a6454eef1205cc217e48e9c5cb648cb09c30fbae
Anghela Lizarro <anghelalizarro7@gmail.com>
2019-08-05 00:57:39 -0400

    Pokedex with Main

---
+++ LizarroAnghela/Pokedex2/src/Attack.java
@@ -0,0 +1,17 @@
+public class Attack {
+    private Type type;
+    private String name;
+    private Integer damage;
+
+    public Attack(Type type, String name, Integer damage) {
+        this.type = type;
+        this.name = name;
+        this.damage = damage;
+    }
+
+
+
+
+
+    //metodo calculate the damage
+}

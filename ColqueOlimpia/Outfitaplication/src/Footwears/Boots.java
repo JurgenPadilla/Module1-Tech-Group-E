fcdb865415d8e81f78a8f58f6fc698ac3cc0e3b0
Olimpia <olimpia.colque.jala@gmail.com>
2019-08-09 11:23:14 -0400

    Outfit aplication Final

--- ColqueOlimpia/Outfitaplication/src/Footwears/Boots.java
+++ ColqueOlimpia/Outfitaplication/src/Footwears/Boots.java
@@ -5,5 +5,17 @@ public class Boots extends Footwear {
     public Boots(String color, String size, String brand, boolean isFormal, String fabrique, String typeBoots ) {
         super(color, size, brand, isFormal, fabrique);
         this.typeBoots = typeBoots;
+
+    }
+    public String getTypeBootst() {
+        return typeBoots;
+    }
+
+    public void setTypeBoots(String typeBoots) {
+        this.typeBoots = typeBoots;
+    }
+
+    public String toString (){
+        return "Boots features " + this.typeBoots + " " + this.color + " "+this.brand + " " + this.size;
     }
 }
